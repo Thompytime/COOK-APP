4b81d23a810a800570f902cb6b09237d22d42c42
ThompyTime <christopherjohnthompson_9@outlook.com>
2025-05-09 23:06:27 +0100

    Remove Supabase console logs

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -4,7 +4,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 
-console.log('URL:', supabaseUrl)
-console.log('Anon Key:', supabaseAnonKey)
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
